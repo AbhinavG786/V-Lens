27f1669825f53749f29f05bcb7687a154bf8077e
AbhinavGupta <bengupta786@gmail.com>
2025-06-26 17:13:51 +0530

    fix cart schema

--- src/models/cartModel.ts
+++ src/models/cartModel.ts
@@ -1,17 +1,7 @@
 import mongoose, { Schema, InferSchemaType, model } from "mongoose";
-import { v4 as uuidv4 } from "uuid";
 
-const cartSchema = new Schema(
+const cartItemSchema = new Schema(
   {
-    _id: {
-      type: String,
-      default: uuidv4, 
-    },
-    userId: {
-      type: String,
-      required: true,
-      ref: "User", 
-    },
     productId: {
       type: Schema.Types.ObjectId,
       ref: "Product",
@@ -23,10 +13,26 @@ const cartSchema = new Schema(
       min: 1,
     },
   },
+  { _id: false }
+);
+
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      ref: "User",
+      index: true,
+    },
+    items: {
+      type: [cartItemSchema],
+      default: [],
+    },
+  },
   { timestamps: true }
 );
 
-cartSchema.index({ userId: 1, productId: 1 }, { unique: true });
+cartSchema.index({ userId: 1 }, { unique: true });
 
 type CartType = InferSchemaType<typeof cartSchema>;
 
