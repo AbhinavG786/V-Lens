7bf4a8caee41ade81dc5c7b66cda8b44612c7874
Yuvraj <yuvrajbansal30dec@gmail.com>
2025-06-27 18:32:51 +0530

    add wishlist schema

---
+++ src/models/wishlistSchema.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, InferSchemaType } from "mongoose";
+
+const wishlistSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product", 
+          required: true,
+        },
+        addedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+type WishlistType = InferSchemaType<typeof wishlistSchema>;
+
+export const Wishlist = mongoose.model<WishlistType>("Wishlist", wishlistSchema);
