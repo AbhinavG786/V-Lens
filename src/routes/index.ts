4c5f3b7c6c72a067f065675e37231e2af6dc97fd
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-06-27 10:13:38 +0530

    [Siddharth] ADD:Added Subscription model and Notification APIs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import userRoutes from "./userRoutes"
 import productRoutes from "./productRoutes"
+import notificationRoutes from "./notificationRoutes";
 import { Router } from "express"
 
 const router = Router();
 
 router.use("/user", userRoutes);
 router.use("/product", productRoutes);
+router.use("/notification",notificationRoutes);
 
 export default router;
