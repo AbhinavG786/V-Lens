00146a6c9730fd5c929da779fe06589fbb4568a1
Ojal Rane <89273608+OjalRane@users.noreply.github.com>
2025-06-29 14:09:51 +0530

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,7 +11,7 @@ import inventoryRoutes from "./inventoryRoutes";
 import addressRoutes from "./addressRoutes";
 import roomRoutes from "./roomRoutes";
 import wishlistRoutes from "./wishlistRoutes";
-
+import reviewRoutes from "./reviewRoutes";
 
 const router = Router();
 
@@ -27,5 +27,6 @@ router.use("/admin", inventoryRoutes);
 router.use("/address", addressRoutes);
 router.use("/room", roomRoutes);
 router.use("/wishlist", wishlistRoutes);
+router.use("/reviews", reviewRoutes);
 
 export default router;
