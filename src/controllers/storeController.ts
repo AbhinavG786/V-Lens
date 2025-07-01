961ea0de758f4a6849d977adf44d0c1c0c6721a3
AbhinavGupta <bengupta786@gmail.com>
2025-07-01 22:50:31 +0530

    fix store controller

--- src/controllers/storeController.ts
+++ src/controllers/storeController.ts
@@ -3,7 +3,8 @@ import Store from '../models/storeModel';
 
 export const findNearby = async (req: express.Request, res: express.Response) => {
   const { lat, lng, radius = 5000 } = req.query;
-  if (!lat || !lng) return res.status(400).json({ error: 'Provide lat and lng' });
+  if (!lat || !lng) { res.status(400).json({ error: 'Provide lat and lng' });
+  return}
 
   const nearby = await Store.find({
     location: {
