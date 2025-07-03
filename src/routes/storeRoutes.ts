310313486c82e73edd7d4e36eeb5485e3f31aa3a
PVSVASANTH2004 <ponukumativasanth@gmail.com>
2025-07-03 10:11:03 +0530

    Added new Feature

--- src/routes/storeRoutes.ts
+++ src/routes/storeRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { findNearby } from '../controllers/storeController';
+import paginationMiddleware from "../middlewares/paginationMiddleware";
 const router = Router();
 
 router.get('/stores/nearby', findNearby);
+
+router.route("/user/:userId").get(paginationMiddleware(10, 50), wishlist.getUserWishlist);
 
 export default router;
