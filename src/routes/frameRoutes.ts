d84bfa6c070d44a2ef8ed280a5a83a24467db2ce
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-07-09 20:52:51 +0530

    [FIX]:Verified gender in updateFrame API and authenticated frame routes

--- src/routes/frameRoutes.ts
+++ src/routes/frameRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import frameController from "../controllers/frameController";
 import upload from "../middlewares/upload";
+import adminAuth from "../middlewares/adminAuth"
 
 const router = Router();
 
-router.post("/create",upload.single("file"), frameController.createFrame);
+router.post("/create",adminAuth.verifyAdminSession,upload.single("file"), frameController.createFrame);
 router.get("/all", frameController.getAllFrames);
 router.get("/:id", frameController.getFrameById);
-router.put("/:id",upload.single("image"), frameController.updateFrame);
-router.delete("/:id", frameController.deleteFrame);
+router.put("/:id",adminAuth.verifyAdminSession,upload.single("image"), frameController.updateFrame);
+router.delete("/:id",adminAuth.verifyAdminSession, frameController.deleteFrame);
 
 export default router;
