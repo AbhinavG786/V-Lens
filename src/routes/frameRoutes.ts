fa63112bd035664cf16732749395e83d29c29547
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-07-09 17:51:44 +0530

    [Siddharth] FIX: Fixed frame controllers

--- src/routes/frameRoutes.ts
+++ src/routes/frameRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import frameController from "../controllers/frameController";
+import upload from "../middlewares/upload";
 
 const router = Router();
 
-router.post("/create", frameController.createFrame);
+router.post("/create",upload.single("file"), frameController.createFrame);
 router.get("/all", frameController.getAllFrames);
 router.get("/:id", frameController.getFrameById);
-router.put("/:id", frameController.updateFrame);
+router.put("/:id",upload.single("image"), frameController.updateFrame);
 router.delete("/:id", frameController.deleteFrame);
 
 export default router;
