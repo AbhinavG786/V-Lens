88fba678fa8d8168a5acc59ee5f338a810394212
PVSVASANTH2004 <ponukumativasanth@gmail.com>
2025-07-18 19:36:55 +0530

    Updated the Readme File and Also RecommendationController API

--- src/controllers/recommendationController.ts
+++ src/controllers/recommendationController.ts
@@ -4,18 +4,7 @@ import { Product } from "../models/productModel";
 import { Wishlist } from "../models/wishlistModel";
 import { Order } from "../models/orderModel";
 import { Review } from "../models/reviewModel";
-import Razorpay from 'razorpay';
 
-let razorpay: Razorpay | null = null;
-
-if (process.env.RAZORPAY_KEY_ID && process.env.RAZORPAY_SECRET) {
-  razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_SECRET,
-  });
-} else {
-  console.warn('Razorpay not initialized: missing credentials');
-}
 
 export { razorpay };
 
